d73599dba6377276ffe171e54cc99fe81aac487e
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-04-20 22:46:13 -0700

    added image_preprocess to manage loading of mask landmark points and image

---
+++ image_preprocess.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+import numpy as np
+
+
+def imgPreprocess():
+	
+	mask_path = "./Day_of_the_Dead/muertos_mask.png"
+	mask = cv.imread(mask_path, cv.IMREAD_UNCHANGED)
+	
+	# split mask into color and alpha channels
+	b,g,r,a = cv.split(mask)
+	mask = cv.merge((b,g,r))
+	mask = np.float32(mask)/255
+	
+	alpha = cv.merge((a,a,a))
+	alpha = np.float32(alpha)
+	
+	return (mask, alpha)
+
+
+def pointsPreprocess():
+	
+	mask_pts_path = "./Day_of_the_Dead/muertos_points.txt"
+	
+	mask_pts = []
+	lines = np.loadtxt(mask_pts_path, dtype='uint16')
+	
+	for p in lines:
+		mask_pts.append((p[0], p[1]))
+	
+	return mask_pts
+	
+	
+
+
+
