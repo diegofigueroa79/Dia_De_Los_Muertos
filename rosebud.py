25fdecf6c2b2aac221ad48458e21c8c18ca29df0
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-04-29 23:12:36 -0700

    made list of corresponding pair of triangles for rose_crown and profile

--- rosebud.py
+++ rosebud.py
@@ -19,7 +19,7 @@ def get_saved_pts(notepad):
 	
 	return crown_points
 
-points = get_saved_pts(notepad)
+crown_points = get_saved_pts(notepad)
 
 crown = cv.imread("rose_crown.png", cv.IMREAD_UNCHANGED)
 
@@ -32,10 +32,16 @@ bottom_right = landmark_pts[16]
 top_left = (bottom_left[0], bottom_left[1] - pt_diff)
 top_right = (bottom_right[0], bottom_right[1] - pt_diff)
 
+
+crown_triangles = [[crown_points[0], crown_points[2], crown_points[3]], [crown_points[0], crown_points[1], crown_points[3]]]
+face_triangles = [[bottom_left, top_left, top_right], [bottom_left, bottom_right, top_right]]
+
+
 def show(image):
 	cv.imshow('im', image)
 	cv.waitKey(0)
 	cv.destroyAllWindows()
+	
 
 
 
